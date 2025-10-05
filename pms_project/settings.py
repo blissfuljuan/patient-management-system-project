293b24dc13d6abb74283ffa1ba331c027853b124
blissfuljuan <cirevil.1989@gmail.com>
2025-10-05 20:34:51 +0800

    technical(project): add app base html files and tailwind settings

--- pms_project/settings.py
+++ pms_project/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'pms_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIR = BASE_DIR / 'static'
+STATICFILES_DIR = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
