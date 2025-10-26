2e12cd287c7d7cff7192985280227a7a0f0a3959
blissfuljuan <cirevil.1989@gmail.com>
2025-10-26 16:27:59 +0800

    feature: apply new UI design to templates

--- patients/urls.py
+++ patients/urls.py
@@ -6,5 +6,6 @@ app_name = 'patients'
 urlpatterns = [
     path('', views.PatientListView.as_view(), name='list'),
     path('new/', views.PatientCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.PatientDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.PatientUpdateView.as_view(), name='edit'),
 ]
